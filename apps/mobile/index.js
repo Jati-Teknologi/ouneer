12c48ca85f5f014aa6d74ddcf9139d2975b9df4f
Risqi Romadhoni <risqi.romadhoni96@gmail.com>
2024-01-11 15:48:13 +0700

    chore: initiall commit

---
+++ apps/mobile/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import main from './src';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => main);
